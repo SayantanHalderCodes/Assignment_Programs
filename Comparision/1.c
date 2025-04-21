5351af7a48e7b034aca9e6ddd6eeac42d56fdcbb
Sayantan Halder <sayantanhalder27052005@gmail.com>
2025-04-21 21:15:13 +0530

    Update 1.c

--- Comparision/1.c
+++ Comparision/1.c
@@ -1,21 +1,14 @@
 /*Write a program to compare two numbers using the conditional operator and display the larger number. Using
 the same conditional operator write a program to find the largest of three numbers entered by the user. Depict
 the logic using a flowchart.*/
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int a,b;
+    int a, b, c;
     printf("Enter the first number: ");
-    scanf("%d",&a);
+    scanf("%d", &a);
     printf("Enter the second number: ");
-    scanf("%d",&b);
-    if (a>b)
-    {
-        printf("The number '%d' is greater than '%d'.",a,b);
-    }else if(a==b){
-        printf("The number '%d' is equal to '%d'.",a,b);
-    }else{
-        printf("The number '%d' is greater than '%d'.",b,a);
-    }
+    scanf("%d", &b);
+    c = a > b ? printf("The greater number is '%d'.", a) : printf("The greater number is '%d'.", b);
     return 0;
 }
